fa142596e9355694e873245b39ba85fcb0c878c0
chocoladisco <chocoladisco@gmail.com>
2015-04-08 15:43:57 +0200

    added callback

--- festival.js
+++ festival.js
@@ -1,5 +1,5 @@
 var exec = require('child_process').exec;
 
-module.exports.toSpeech = function(text, filename) {
-	return exec('echo ' + text + ' | text2wave | lame - ' + filename);
+module.exports.toSpeech = function(text, filename, callback) {
+	exec('echo ' + text + ' | text2wave | lame - ' + filename , callback);
 };
